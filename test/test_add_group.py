bcd782956ca0d3a6e3c740e8107dcf5eeb8c47bc
AVaskovych <32718833+AVaskovych@users.noreply.github.com>
2017-11-12 23:54:04 +0200

    Модификация групп, модификация фикстурі

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,15 +3,7 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="sadasfdsf", header="sadasfds", footer="asdsfs"))
-    app.session.logout()
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
-
-
-if __name__ == '__main__':
-    unittest.main()
