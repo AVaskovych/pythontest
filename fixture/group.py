48c74ef69ac8f7058dadd22828448a01fa4c4e68
AVaskovych <32718833+AVaskovych@users.noreply.github.com>
2017-11-13 04:19:32 +0200

    добавлены проверки

--- fixture/group.py
+++ fixture/group.py
@@ -1,4 +1,4 @@
-
+from model.group import Group
 
 
 class GroupHelper:
@@ -8,6 +8,8 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
+        if wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0:
+            return
         wd.find_element_by_link_text("groups").click()
 
     def change_field_value(self, field_name, text):
@@ -64,3 +66,16 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name=text, id = id))
+        return groups
+
+
+
